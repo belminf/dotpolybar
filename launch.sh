9b98940e48c49d43d565f25675c8fd6017c85518
Belmin Fernandez <belminf@gmail.com>
2017-10-02 20:27:48 -0400

    fixed env variable, loop, added secondary mons

--- launch.sh
+++ launch.sh
@@ -8,7 +8,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Get monitor information
 connected_monitors=($(xrandr --query | grep " connected" | cut -d" " -f1))
-primary_monitor=($(xrandr --query | grep " connected primary" | cut -d" " -f1))
+primary_monitor="$(xrandr --query | grep " connected primary" | cut -d" " -f1)"
 
 # If no primary, default to first
 if [ -z "$primary_monitor" ]
@@ -17,13 +17,13 @@ then
 fi
 
 # Loop through each monitor
-for m in $connected_monitors
+for m in ${connected_monitors[@]}
 do
 
   # Choose the bar
   [[ "$m" -eq "$primary_monitor" ]] && bar="primary" || bar="others"
   
   # Bring up the bar
-  MONITOR=$m polybar --reload $bar &
+  monitor="$m" polybar --reload $bar &
 
 done
